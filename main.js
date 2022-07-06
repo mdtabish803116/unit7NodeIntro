a731b180ff16645998fc45b7b3480e1b3426e599
mdtabish803116 <mdtabish803116@gmail.com>
2022-07-06 23:30:58 +0530

    added modified files

---
+++ main.js
@@ -0,0 +1,9 @@
+const add = require("./add");
+const substract = require("./substract");
+const divide = require("./divide");
+const multiply = require("./multiply");
+
+console.log(add(2 , 3));
+console.log(substract(2 , 3));
+console.log(divide(2 , 3));
+console.log(multiply(2 , 3));
